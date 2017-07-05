4aefa3cbfad12f937107e5208aa4ddb99b61acc6
Arvid @ soundlab PC <arvid@arvidjense.com>
2017-07-05 20:13:40 +0200

    fix of 6 tone

--- orbiphoneMuziekgebouwAantIJ/ledStuff.ino
+++ orbiphoneMuziekgebouwAantIJ/ledStuff.ino
@@ -1,19 +1,22 @@
 elapsedMillis ledUpdateTimer;
 
-int colorCycleTime= 10000 /255 + TONESAMOUNT;//ms per cycle, added tonesamount to make a variation between different tonepads
+int colorCycleTime = 10000 / 255 + TONESAMOUNT; //ms per cycle, added tonesamount to make a variation between different tonepads
 int colorDistance = 5;// in hue steps
 
 void ledUpdate() {
   if (ledUpdateTimer > ledUpdateTime) {
     ledUpdateTimer = 0;
 
-    float mappedTotalAverage=fmap( totalAverage,0,.1,0,1);
+    float mappedTotalAverage = fmap( totalAverage, 0, .1, 0, 1);
 
 
-    leds[0][0] = CHSV( (millis()/colorCycleTime)%255                , 255*buttonState[1], 255*mappedTotalAverage);
-    leds[1][0] = CHSV( (millis()/colorCycleTime)%255                , 255*buttonState[2], 255*mappedTotalAverage);
-    leds[2][0] = CHSV( (millis()/colorCycleTime)%255                , 255*buttonState[3], 255*mappedTotalAverage);
+    leds[0][0] = CHSV( (millis() / colorCycleTime) % 255                , 255 * buttonState[1], 255 * mappedTotalAverage);
+    leds[1][0] = CHSV( (millis() / colorCycleTime) % 255                , 255 * buttonState[2], 255 * mappedTotalAverage);
+    leds[2][0] = CHSV( (millis() / colorCycleTime) % 255                , 255 * buttonState[3], 255 * mappedTotalAverage);
     FastLED.show();
 
   }
 }
+
+//mapping the position of where is touched, to a hue value (0-255)
+//how to do this?
--- orbiphoneMuziekgebouwAantIJ/orbiphoneMuziekgebouwAantIJ.ino
+++ orbiphoneMuziekgebouwAantIJ/orbiphoneMuziekgebouwAantIJ.ino
@@ -1,21 +1,21 @@
-#define TONESAMOUNT 7 // amount of pads active
+#define TONESAMOUNT 6 // amount of pads active
 float sineMaxAmplitude = 1.0/TONESAMOUNT + .03; //about one divided by the amount of tones available (.11 is the safest value)
 
 #define debugUpdateTime 250//in ms
 #define audioUpdateTime 100//in us   =1ms
 #define ledUpdateTime 10//in ms    10ms = 100Hz
-#define buttonUpdateTime 5//in ms
+#define buttonUpdateTime 1//in ms
 #define calibrationUpdateTime 5//in ms
 
 #define lowThreshold 5 //a value to reach before the thing gets triggered
 #define maxRange 250 //the maximum range of the sensors
 #define glide 10    //glide time for frequency change
 
-#define numReadings 50 // running average of the sensor values
+#define numReadings  75// running average of the sensor values
 
 #define autoCalibTime 3000 //ms how long the values should be stable before autoCalib
-#define autoCalibRange 10 // how stable the values should be for the timer to start
-#define autoCalibSoundRange 0.01 //how much the volume should stay the same for recalibration
+#define autoCalibRange 20 // how stable the values should be for the timer to start
+#define autoCalibSoundRange 0.02 //how much the volume should stay the same for recalibration
 #define calibCycles 50 // it will average this amount of cycles when calibrating
 
 
