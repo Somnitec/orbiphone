26700695e4b10476beead96dccff1824a0cff044
Arvid <arvid@arvidandmarie.com>
2020-09-26 18:04:44 +0200

    added whole tone scale

--- orbiphone/audioStuff.ino
+++ orbiphone/audioStuff.ino
@@ -1,11 +1,12 @@
 
 elapsedMicros audioUpdateTimer;
 
-int toneSets = 3;
+int toneSets = 4;
 int toneSet[][11] = {
   {NOTE_C5, NOTE_A4, NOTE_G4, NOTE_E4, NOTE_D4, NOTE_C4, NOTE_A3, NOTE_G3, NOTE_E3, NOTE_D3, NOTE_C3}, //pentatonic C3
   {NOTE_F4, NOTE_E4, NOTE_D4, NOTE_C4, NOTE_B3, NOTE_A3, NOTE_G3, NOTE_F3, NOTE_E3, NOTE_D3, NOTE_C3}, //diatonic C3
   {NOTE_C4, NOTE_F3, NOTE_E3, NOTE_DS3, NOTE_A3, NOTE_G3, NOTE_D3, NOTE_B3, NOTE_GS3, NOTE_CS3, NOTE_AS3}, //chromatic C3
+  {NOTE_C5, NOTE_AS4, NOTE_GS4, NOTE_FS4, NOTE_E4, NOTE_D4, NOTE_C3, NOTE_AS3, NOTE_GS3, NOTE_FS3, NOTE_E3}, //whole tone C3
 };
 
 
